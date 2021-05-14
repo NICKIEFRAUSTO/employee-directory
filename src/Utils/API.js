1c414b5a528c191eb5576c1dd024db923adcdc23
NICKIEFRAUSTO <79732055+NICKIEFRAUSTO@users.noreply.github.com>
2021-05-14 13:44:17 -0500

    creted function for the api call to https://randomuser.me in utils/api.js

--- src/Utils/API.js
+++ src/Utils/API.js
@@ -1,1 +1,9 @@
 import axios from "axios";
+// ---------randome user API---------------------------------//
+const BASEURL = "https://randomuser.me/api/?results=200&nat=us&inc=picture,name,email,phone";
+// ----------included ?results=200 to limit the returned list to 200 employees, nat=us - returns only employees from the us, inc=... limits the returned fields to only those listed------------------------------------------------------//
+export default {
+    search: function() {
+      return axios.get(BASEURL);
+    }
+  };
