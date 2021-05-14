1f7ae498e86280cf1a70c263719606926deb6dd0
NICKIEFRAUSTO <79732055+NICKIEFRAUSTO@users.noreply.github.com>
2021-05-14 14:06:50 -0500

    created searchresultcontainer.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import SearchResultContainer from "./components/SearchResultContainer";
+import SearchResultContainer from "./Components/SearchResultContainer";
 
 function App() {
   return <SearchResultContainer />;
---
+++ src/Components/SearchResultContainer.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+import SearchForm from "./SearchForm";
+import ResultList from "./ResultList";
+import API from "../Utils/API";
+
+class SearchResultContainer extends Component {
+  state = {
+    search: "",
+    results: []
+  };
+
+  //----------------------- When this component mounts, search the randomeuser.me API for list of employees------------------------------//
+  componentDidMount() {
+    this.searchEmployee("");
+  }
+
+  searchEmployee = () => {
+    API.search()
+      .then(res => this.setState({ results: res.data.results }))
+      .catch(err => console.log(err));
+  };
+
+  handleInputChange = event => {
+    const name = event.target.name;
+    const value = event.target.value;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  //---------------------- When the form is submitted, search the randomuser.me API for `this.state.search`---------------------//
+  handleFormSubmit = event => {
+    event.preventDefault();
+    this.searchEmployee(this.state.search);
+  };
+  
+// -----------------------------------render list to the page----------------------------------------//
+  render() {
+    return (
+      <div>
+        <SearchForm
+          search={this.state.search}
+          handleFormSubmit={this.handleFormSubmit}
+          handleInputChange={this.handleInputChange}
+        />
+        <ResultList results={this.state.results}
+          search={this.state.search}
+         />
+      </div>
+    );
+  }
+}
+
+export default SearchResultContainer;
